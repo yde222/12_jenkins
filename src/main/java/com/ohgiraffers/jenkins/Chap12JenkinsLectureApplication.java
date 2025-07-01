7d02513d1de9eaef27af5c317d813d686ad21404
macacolabs <monkey.ohgiraffers@gmail.com>
2025-07-01 10:31:38 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/jenkins/Chap12JenkinsLectureApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap12JenkinsLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap12JenkinsLectureApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -0,0 +1,33 @@
+package com.ohgiraffers.jenkins.controller;
+
+import com.ohgiraffers.jenkins.dto.CalculatorDto;
+import com.ohgiraffers.jenkins.service.CalculatorService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm alive!😁";
+    }
+
+    @GetMapping("/plus")
+    public ResponseEntity<CalculatorDto> plusTwoNumbers(CalculatorDto calculatorDto) {
+        log.info("핸들러 실행 여부 및 값 확인 : {}", calculatorDto);
+
+        /* Service 계층으로 기능 요청 */
+        int result = calculatorService.plusTwoNumbers(calculatorDto);
+
+        calculatorDto.setSum(result);
+        return ResponseEntity.ok(calculatorDto);
+    }
+}
